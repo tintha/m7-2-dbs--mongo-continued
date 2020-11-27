68e15c42a5874fcdfca2cb2fa3c8eb5e8bf8e2c9
tintha <tintha@gmail.com>
2020-11-27 11:24:46 -0500

    Cleaned up

--- server/routes.js
+++ server/routes.js
@@ -6,20 +6,6 @@ const {
   updateCustomer,
 } = require("./handlers");
 
-("use strict");
-const { MongoClient } = require("mongodb");
-
-require("dotenv").config();
-const { MONGO_URI } = process.env;
-
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-const NUM_OF_ROWS = 8;
-const SEATS_PER_ROW = 12;
-
 router.get("/api/seat-availability", getSeats);
 
 router.post("/api/book-seat/", bookSeat);
